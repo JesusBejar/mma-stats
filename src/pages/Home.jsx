b13cce00ccaf373c42bf524bf0347b4851dad18a
Jesus Bejar <israelbejarii@gmail.com>
2025-08-04 09:02:14 -0600

    added simple jsx to home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>MMA Stats</h1>
+      <p>find by:</p>
+      <ul>
+        <li>Fighter Name</li>
+        <li>UFC #</li>
+      </ul>
+      <button>Search</button>
+    </div>
+  );
+};
+
+export default Home;
