256e636b57ebc4465537709173f5a28d64a688f7
Jesus Bejar <israelbejarii@gmail.com>
2025-08-04 10:03:39 -0600

    added form and input to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,32 @@
+import fighter from "../api/fighter";
+import getFighterByName from "../api/fighter";
+import React, { useState } from "react";
+
 const Home = () => {
+  const { fighterName, setFighterName } = useState("");
+  const API_KEY = "9fa47c65e6msh6013516bc42c8bbp177bd0jsnfe677f674a14";
   return (
     <div>
       <h1>MMA Stats</h1>
       <p>find by:</p>
       <ul>
-        <li>
-          <button>Fighter Name</button>
-        </li>
+        <form onSubmit={getFighterByName}>
+          <li>
+            <button>Fighter Name</button>
+            <input
+              type="text"
+              placeholder="Enter fighter name"
+              value={fighterName}
+              onChange={setFighterName(e.target.value)}
+            />
+          </li>
+        </form>
         <li>
           <button>UFC #</button>
+          <input type="text" placeholder="Enter UFC #" />
         </li>
       </ul>
-      <button>Search</button>
+      <button type="submit">Search</button>
     </div>
   );
 };
