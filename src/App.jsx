4b38bcad8c911ed23ab6b8603e4001bc09f3f8f4
Jesus Bejar <israelbejarii@gmail.com>
2025-08-04 09:01:59 -0600

    added switch case w revelant routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Component, useState } from "react";
+import Home from "./pages/Home";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
-const FighterCard = ({ name, image, description }) => {
-  return (
-    <div className="fighter-card">
-      <img src={image} alt={`${name} image`} className="fighter-image" />
-      <h2 className="fighter-name">{name}</h2>
-      <p className="fighter-description">{description}</p>
-    </div>
-  );
-};
 const App = () => {
+  let Component;
+  switch (window.location.pathname) {
+    case "/home":
+      document.title = "Home";
+      Component = Home;
+      break;
+    case "/fighters":
+      document.title = "Fighters";
+      Component = FightCard;
+      break;
+    case "/events":
+      document.title = "UFC Events";
+      Component = UFCCard;
+      break;
+    default:
+      document.title = "MMA Stats";
+  }
   return (
-    <div className="App">
-      <FighterCard
-        name="Conor McGregor"
-        img="https://www.mensjournal.com/.image/t_share/MTk2MTM2OTI5NTk5NjkzOTY5/main2-conor.jpg"
-        description="Irish mixed martial artist and former UFC champion."
-      />
-    </div>
+    <>
+      <NavBar />
+      <Component />
+    </>
   );
 };
 
