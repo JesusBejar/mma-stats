689c770967fb3c6ae0b15dd7d597d576973355d9
Jesus Bejar <israelbejarii@gmail.com>
2025-08-08 11:08:42 -0600

    changed logic from ufc event to year

--- src/api/fighter.js
+++ src/api/fighter.js
@@ -1,20 +1,20 @@
-const getUFCEvent = async (ufcNumber, API_KEY) => {
-  const url = `https://mma-stats.p.rapidapi.com/ufc_event?ufc=${ufcNumber}`;
+const getUFCYear = async (year, API_KEY) => {
+  const url = `https://mma-stats.p.rapidapi.com/fights_by_year?year=${year}`;
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "x-rapidapi-key": API_KEY,
-      "x-rapidapi-host": "mma-stats.p.rapidapi.com",
-    },
+      'x-rapidapi-key': '9fa47c65e6msh6013516bc42c8bbp177bd0jsnfe677f674a14',
+      'x-rapidapi-host': 'mma-stats.p.rapidapi.com'
+    }
   };
-
+  
   try {
     const response = await fetch(url, options);
     const result = await response.text();
+    console.log(result);
     return result;
   } catch (error) {
     console.error(error);
-    return null;
   }
 };
 
